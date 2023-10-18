2ea21aede3c6d3d3f645c2b1235599ec02dabf4f
Daniel Nunes <danielmnunes08@gmail.com>
2023-10-18 16:15:33 -0300

    submit lasagna

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,23 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int time) {
+        return 40 - time;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
 
     // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int a) {
+        return a * 2;
+    }
+
+    public int totalTimeInMinutes(int a, int b) {
+        return b + preparationTimeInMinutes(a);
+    }
 
     // TODO: define the 'totalTimeInMinutes()' method
 }
